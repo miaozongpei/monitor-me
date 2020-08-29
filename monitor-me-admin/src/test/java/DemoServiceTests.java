8aab6e5b228d0aeb63ee91a5185543e5b03c2862
miaozongpei <miaozongpei@163.com>
2020-08-29 21:00:37 +0800

    模拟插入

--- monitor-me-admin/src/test/java/DemoServiceTests.java
+++ monitor-me-admin/src/test/java/DemoServiceTests.java
@@ -1,7 +1,12 @@
 import com.alibaba.fastjson.JSON;
 import com.m.monitor.me.admin.MonitorMeAdminApplication;
 import com.m.monitor.me.service.mogodb.norm.MethodPointService;
+import com.m.monitor.me.service.mogodb.norm.NormMinuteService;
+import com.m.monitor.me.service.transfer.server.norm.TimeNorm;
+import com.m.monitor.me.service.transfer.server.record.IntegratorNormRecord;
 import com.m.monitro.me.common.enums.MonitorTimeUnitEnum;
+import com.m.monitro.me.common.utils.DoubleUtil;
+import com.m.monitro.me.common.utils.MonitorTimeUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,6 +31,21 @@ public class DemoServiceTests {
 		List<double[]> monitorNorm=methodPointService.queryRealTimeNorm("10.249.243.155",
 				null,20200821153453L, MonitorTimeUnitEnum.MINUTE,60);
 		System.out.println(JSON.toJSON(monitorNorm));
+	}
+
+	@Resource
+	private NormMinuteService normMinuteService;
+
+	@Test
+	public void saveTest() {
+		for(int i=1;i<=48*60;i++) {
+			IntegratorNormRecord record = new IntegratorNormRecord("monitor-me-example", "127.0.0.1");
+			TimeNorm timeNorm = new TimeNorm(MonitorTimeUtil.subTime(20200829202700L, 1*i, MonitorTimeUnitEnum.MINUTE));
+			timeNorm.setAvg(DoubleUtil.avg(Math.random() * 50, 1));
+			timeNorm.setTotal(5000);
+			record.getTs().add(timeNorm);
+			normMinuteService.save(record);
+		}
 	}
 
 }
