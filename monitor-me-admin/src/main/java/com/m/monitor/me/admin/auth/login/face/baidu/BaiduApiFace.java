cb6e9e5a0fbb633c6df4c58ba981f9e3345b6d78
miao <zongpei.miao@opay-inc.com>
2020-10-22 21:20:34 +0800

    login 人脸登录

--- monitor-me-admin/src/main/java/com/m/monitor/me/admin/auth/login/face/baidu/BaiduApiFace.java
+++ monitor-me-admin/src/main/java/com/m/monitor/me/admin/auth/login/face/baidu/BaiduApiFace.java
@@ -1,21 +1,36 @@
 package com.m.monitor.me.admin.auth.login.face.baidu;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.baidu.aip.face.AipFace;
-import com.baidu.aip.http.AipRequest;
-import com.baidu.aip.http.EBodyFormat;
-import org.json.JSONObject;
+import com.m.monitro.me.common.utils.HttpUtil;
+import lombok.extern.slf4j.Slf4j;
 
+
+import java.math.BigDecimal;
+import java.net.URLEncoder;
+import java.util.Map;
+@Slf4j
 public class BaiduApiFace extends AipFace{
     public BaiduApiFace(String appId, String apiKey, String secretKey) {
         super(appId, apiKey, secretKey);
     }
-    public JSONObject identify(String image, String groupId) {
-        AipRequest request = new AipRequest();
-        this.preOperation(request);
-        request.addBody("image", image);
-        request.addBody("group_id", groupId);
-        request.setUri("https://aip.baidubce.com/rest/2.0/face/v2/identify?access_token="+this.accessToken);
-        request.setBodyFormat(EBodyFormat.RAW_JSON);
-        return this.requestServer(request);
+    public Boolean identify(String image,String groupId) {
+        this.getAccessToken(this.config);
+        // 请求url
+        String url = "https://aip.baidubce.com/rest/2.0/face/v2/identify";
+        try {
+            String param = "group_id=" + groupId + "&image=" +  URLEncoder.encode(image, "UTF-8"); ;
+            String result = HttpUtil.post(url, this.accessToken, param);
+            Map<String, Object> map = JSON.parseObject(result);
+            JSONArray r= (JSONArray) map.get("result");
+            System.out.println(r);
+            com.alibaba.fastjson.JSONObject x=r.getJSONObject(0);
+            BigDecimal scores=(BigDecimal)((JSONArray)x.get("scores")).get(0);
+            return scores.doubleValue()>=80;
+        } catch (Exception e) {
+            log.error("人脸识别登录异常：",e);
+            return false;
+        }
     }
 }
