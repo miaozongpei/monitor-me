dda227dbe07c7f2f1e414b140a9889c4e560d53c
miao <zongpei.miao@opay-inc.com>
2021-09-10 18:33:35 +0800

    拦截异常补偿

--- monitor-me-client/src/main/java/com/m/monitor/me/client/point/MonitorInterceptor.java
+++ monitor-me-client/src/main/java/com/m/monitor/me/client/point/MonitorInterceptor.java
@@ -6,8 +6,10 @@ import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
 
 import javax.annotation.Resource;
+
 /**
  * 监控点拦截器
+ *
  * @Author: miaozp
  * @Date: 2020/10/31 6:24 下午
  **/
@@ -15,15 +17,26 @@ import javax.annotation.Resource;
 public class MonitorInterceptor implements MethodInterceptor {
     @Resource
     private MonitorHandler monitorHandler;
+
     @Override
     public Object invoke(MethodInvocation invocation) throws Throwable {
-        log.debug("[MonitorMe client] MonitorInterceptor invoke method:{}",invocation.getMethod());
+        log.debug("[MonitorMe client] MonitorInterceptor invoke method:{}", invocation.getMethod());
         Object proceed = null;
-        MonitorContext context=new MonitorContext(invocation.getMethod(),invocation.getArguments());
-        monitorHandler.doBeforeLimit(context);
-        monitorHandler.doBefore(context);
+        MonitorContext context = new MonitorContext(invocation.getMethod(), invocation.getArguments());
+        try {
+            monitorHandler.doBeforeLimit(context);
+            monitorHandler.doBefore(context);
+        } catch (Exception e) {
+            log.error("MonitorInterceptor before error:",e);
+        }
+
         proceed = invocation.proceed();
-        monitorHandler.doAfter(context);
+
+        try {
+            monitorHandler.doAfter(context);
+        } catch (Exception e) {
+            log.error("MonitorInterceptor after error:",e);
+        }
         return proceed;
     }
 }
